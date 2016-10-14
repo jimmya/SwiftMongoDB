9db774898cafff5ca78354a42a012ac7613850dd
Jimmy Arts <arts.jimmy@gmail.com>
2016-10-14 13:41:52 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "MongoDB",
     dependencies: [
         .Package(url: "https://github.com/PureSwift/CBSON.git", majorVersion: 1),
-        .Package(url: "https://github.com/Danappelxx/CMongoC.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/jimmya/CMongoC.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Danappelxx/BinaryJSON", majorVersion: 1, minor: 1)
     ]
 )
